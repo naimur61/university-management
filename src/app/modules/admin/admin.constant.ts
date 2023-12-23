9fec2938e7c088a8a7aafa9108fc8642d19e1044
Naimur <m.naimur61@gmail.com>
2023-12-23 20:44:02 +0600

    admin constant

---
+++ src/app/modules/admin/admin.constant.ts
@@ -0,0 +1,20 @@
+export const BloodGroup = ['A+', 'A-', 'B+', 'B-', 'AB+', 'AB-', 'O+', 'O-'];
+
+export const Gender = ['male', 'female', 'other'];
+
+export const adminSearchableFields = [
+  'id',
+  'email',
+  'contactNo',
+  'bloodGroup',
+  'name.firstName',
+  'name.middleName',
+  'name.lastName',
+];
+export const adminFilterableFields = [
+  'searchTerm',
+  'id',
+  'email',
+  'contactNo',
+  'bloodGroup',
+];
