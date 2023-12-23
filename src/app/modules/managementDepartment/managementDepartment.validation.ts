13cfe083ef8021e4a2f52f86eb686ed3e1ffb1fe
Naimur <m.naimur61@gmail.com>
2023-12-23 18:22:06 +0600

    management zod

---
+++ src/app/modules/managementDepartment/managementDepartment.validation.ts
@@ -0,0 +1,19 @@
+import { z } from 'zod';
+
+const createManagementDepartmentZodSchema = z.object({
+  body: z.object({
+    title: z.string({ required_error: 'Title is required.' }),
+  }),
+});
+
+const updateManagementDepartmentZodSchema = z.object({
+  body: z.object({
+    title: z.string().optional(),
+    ManagementFaculty: z.string().optional(),
+  }),
+});
+
+export const ManagementDepartmentValidation = {
+  createManagementDepartmentZodSchema,
+  updateManagementDepartmentZodSchema,
+};
