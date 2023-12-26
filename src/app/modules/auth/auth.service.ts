37e68961012c925923631ab41f0c1105c5ea046e
Naimur <m.naimur61@gmail.com>
2023-12-26 23:26:48 +0600

    incomplete

--- src/app/modules/auth/auth.service.ts
+++ src/app/modules/auth/auth.service.ts
@@ -1,14 +1,27 @@
+import httpStatus from 'http-status';
+import { ApiError } from '../../../errors/ApiError';
 import { User } from '../user/user.model';
 import { ILoginUser } from './auth.interface';
+import bcrypt from 'bcrypt';
 
 const userLogin = async (payload: ILoginUser) => {
   const { id, password } = payload;
 
-  const isUserExits = await User.find(
+  //   Check User Exist
+  const isUserExit = await User.findOne(
     { id },
     { id: 1, password: 1, isNeedsChangePass: 1 },
-  );
+  ).lean();
+  if (!isUserExit) {
+    throw new ApiError(httpStatus.NOT_FOUND, 'User dose not exit!');
+  }
 
+  //   Check password Matched
+  const isPasswordMatched = await bcrypt.compare(password, isUserExit.password);
+
+  if (!isPasswordMatched) {
+    throw new ApiError(httpStatus.UNAUTHORIZED, 'Password is incorrect!');
+  }
   return {};
 };
 
